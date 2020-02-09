b7baea007dd8892fb4fe1dcc3efbb8528c344682
NetherForce <nederforce@gmail.com>
2020-02-09 10:05:56 +0200

    abe faileve kakwo da wi kasha

--- game.js
+++ game.js
@@ -3,14 +3,12 @@ let myX = 0, myY = 0;
 
 function update() {
 	// Napisanoto tuk se izpulnqva otnovo i otnovo mnogo puti v sekunda
-	myX = myX+(mouseX-myX)/10;
-	myY = myY+(mouseY-myY)/10;
+    
 }
 
 function draw() {
 	// tuk naprogramirai kakvo da se risuva
-	drawImage(backCake, 0, 0, 800, 600);
-	drawImage(femaleAction, myX, myY, 80, 110);
+    
 }
 
 function keyup(key) {
