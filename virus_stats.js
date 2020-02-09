6a4f2e0dab57c1d4b17d99a3af68b7f09dcb3f56
NetherForce <nederforce@gmail.com>
2020-02-09 17:28:21 +0200

    bonus

--- virus_stats.js
+++ virus_stats.js
@@ -6,8 +6,15 @@ class Virus{
         this.lethality = 0;
         this.brInfected = brInfected_;
         this.brDead = 0;
-        this.infectedIn = [0, 0, 0, 0, 0, 0];
-        //Asia=0, severna amerika=1, ushna amerika=2, ewropa=3, afrika=4, awstraliq=5
+        this.infectedIn = [0, 0, 0, 0, 0];
+        this.DNA = 0;
+        this.DNAPerDay = 1;
+        this.bonus={
+            infectivity: 0,
+            severity: 0,
+            lethality: 0
+        };
+        //Ewrasia=0, severna amerika=1, ushna amerika=2, afrika=4, awstraliq=5
     }
     infectivityChange(howMuch_){
         this.infectivity += howMuch_;
@@ -21,5 +28,17 @@ class Virus{
     kill(br_){
         this.brInfected -= br_;
         this.brDead += br_;
+    }
+    addDNA(number_){
+        this.DNA += number_;
+    }
+    updatePerDay(){
+        for(let i=0; i<this.infectedIn.length; i++){
+            let a = Math.random();
+            if(a<(this.infectivity+this.bonus.infectivity)/100){
+                let novozarazeni = this.infectedIn[i] * (1 + Math.random());
+                this.infectedIn[i] += novozarazeni;
+            }
+        }
     }
 }
