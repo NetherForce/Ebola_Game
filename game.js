d9da0ac107822f572c115101f0ef66df617c3c28
GitDaniR <dani.rachev@gmail.com>
2020-02-09 19:22:26 +0200

    Update and create Airports

--- game.js
+++ game.js
@@ -35,9 +35,10 @@ function dayHandler() {
     }
 };
 function draw() {
-    context.fillStyle="black";
-    context.font="20px Georgia";
-    context.fillText(date.getDate() + '.'+ date.getMonth() + '.' + date.getFullYear() + '|' + eventHandler.currentEventName,50,50);
+    context.fillStyle = "black";
+    context.font = "20px Georgia";
+    context.fillText(date.getDate() + '.'+ date.getMonth() + '.' + date.getFullYear() + ' | ' + eventHandler.currentEventName,50,50);
+    for (let curr_airport of airports) curr_airport.draw();
 }
 function keyup(key) {
     console.log("Pressed", key);
