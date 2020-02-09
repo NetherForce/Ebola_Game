05334966e50f2dc5b17ec59db0228defec98eaeb
GitDaniR <dani.rachev@gmail.com>
2020-02-09 22:26:07 +0200

    Add Airports and Airplanes No functionality

--- airports.js
+++ airports.js
@@ -1,5 +1,6 @@
-let airport_img = new Image;
-airport_img.src = 'airport.png';
+let airport_img = new Image, normal_airplane_img = new Image, infected_airplane_img = new Image;
+airport_img.src = 'airport.png'; normal_airplane_img.src = 'normal_airplane.png'; infected_airplane_img.src = 'infected_airplane.png';
+let airport_heigth = 24, airport_width = 60, h_w = airport_width / 2, h_h = airport_heigth / 2;
 class Airport {
   constructor(continent, index, x, y) {
     this.continent = continent;
@@ -8,8 +9,35 @@ class Airport {
     this.y = y;
   }
   draw() {
-    context.drawImage(airport_img, this.x, this.y, 60, 24);
+    context.drawImage(airport_img, this.x, this.y, airport_width, airport_heigth);
+  }
+}
+class Airplane {
+  constructor(startX, startY, endX, endY, speed) {
+    this.x = startX;
+    this.y = startY;
+    this.distance = Math.sqrt((endX - startX) * (endX - startX) + (endY - startY) * (endY - startY));
+    this.speed = speed;
+    this.t = this.distance / this.speed;
+    this.dX = (endX - startX) / this.t;
+    this.dY = (endY - startY) / this.t;
+    this.angle = Math.atan2(endY - startY, endX - startX);
+    if (this.angle < 0) this.angle += 360;
+    this.img = normal_airplane_img;
+  }
+  update() {
+    if (--this.t < 0) return;
+    this.x += this.dX;
+    this.y += this.dY;
+  }
+  draw() {
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(Math.PI / 2 + this.angle);
+    context.drawImage(this.img, 0, 0, 40, 40);
+    context.restore();
   }
 }
 let airports = [new Airport('Eurasia', 0, 520, 170), new Airport('North America', 1, 95, 175),
 new Airport('South America', 2, 200, 320), new Airport('Africa', 3, 365, 245), new Airport('Australia', 4, 665, 355)];
+let airplanes = [];
